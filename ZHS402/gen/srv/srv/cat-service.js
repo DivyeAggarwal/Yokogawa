b8c4cb739b90c00bdbbcd39a5a1d5b96b903c44a
464_0259 <divye.aggarwal@yokogawa.com>
2023-03-21 06:08:52 +0000

    commit bom Display changes

--- ZHS402/gen/srv/srv/cat-service.js
+++ ZHS402/gen/srv/srv/cat-service.js
@@ -374,7 +374,12 @@ module.exports = cds.service.impl(async function (srv) {
 
     this.on('READ', 'DigitPartList', async req => {
         const db = await cds.connect.to('db');
-        const material = await SELECT.from('ZHS402.ZTHBT0001');
+        if(req.query.SELECT.where) {
+            const material = await SELECT.from('ZHS402.ZTHBT0001').where(req.query.SELECT.where); 
+        } else {
+            const material = await SELECT.from('ZHS402.ZTHBT0001');
+        }
+        
 
         let arrayInput = [];
         if (Array.isArray(material)) {
--- ZHS402/srv/cat-service.js
+++ ZHS402/srv/cat-service.js
@@ -374,7 +374,13 @@ module.exports = cds.service.impl(async function (srv) {
 
     this.on('READ', 'DigitPartList', async req => {
         const db = await cds.connect.to('db');
-        const material = await SELECT.from('ZHS402.ZTHBT0001').where(req.query);
+        var material;
+        if(req.query.SELECT.where) {
+            material = await SELECT.from('ZHS402.ZTHBT0001').where(req.query.SELECT.where); 
+        } else {
+            material = await SELECT.from('ZHS402.ZTHBT0001');
+        }
+        
 
         let arrayInput = [];
         if (Array.isArray(material)) {
