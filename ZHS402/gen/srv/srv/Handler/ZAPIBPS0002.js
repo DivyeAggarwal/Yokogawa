23cc5f5328f7f1cf7dfb592f9edbe14d81499f00
464_0259 <divye.aggarwal@yokogawa.com>
2023-05-17 07:28:40 +0000

    commit bom display

--- ZHS402/gen/srv/srv/Handler/ZAPIBPS0002.js
+++ ZHS402/gen/srv/srv/Handler/ZAPIBPS0002.js
@@ -1,4 +1,5 @@
 var registerZAPIBPS0002Handler = function (that, cds) {
+    
     that.on('UPDATE', 'ZCDSEBPS0004', async req => {
         const bupa = await cds.connect.to('db');
         return bupa.run(req.query);
