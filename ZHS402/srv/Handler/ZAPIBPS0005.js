a28cb77ff7997c2ff8ab7a960bdb1765b7b2ff9e
Suresh Babu Achanta <Achanta.SureshBabu@yokogawa.com>
2023-06-06 06:19:01 +0000

    Bug fix

--- ZHS402/srv/Handler/ZAPIBPS0005.js
+++ ZHS402/srv/Handler/ZAPIBPS0005.js
@@ -3,6 +3,9 @@ var registerZAPIBPS0005Handler = function (that, cds) {
     that.on('READ', 'ZCDSEHSDC0009', async req => {
         const bupa = await cds.connect.to('ZSRVBHSD0011');
         return bupa.run(req.query);
-    }); 
+   
 
+    })
+}
+ 
 module.exports = registerZAPIBPS0005Handler;
