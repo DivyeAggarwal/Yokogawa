7c5840a9af9e0b7b444007295496e44cce63c318
464_0259 <divye.aggarwal@yokogawa.com>
2023-06-15 05:18:40 +0000

    8754 change

--- ZHS402/srv/Handler/ZAPIBPS0007.js
+++ ZHS402/srv/Handler/ZAPIBPS0007.js
@@ -1,8 +1,39 @@
 var registerZAPIBPS0007Handler = function (that, cds) {
-    
+
     that.on('READ', 'ZCDSEHPPB0097', async req => {
         const bupa = await cds.connect.to('ZSRVBHPP0019');
         return bupa.run(req.query);
+    });
+    this.on('CREATE', 'ZCDSEHPPB0097', async req => {
+        const soapi = await cds.connect.to('ZSRVBHPP0019');
+        var response = await soapi.tx(req).post("/ZCDSEHPPB0097", req.data);
+        return response;
+    });
+    this.on('CREATE', 'IndividualIssue', async req => {
+        const soapi = await cds.connect.to('ZSRVBHPP0019');
+        var payload = {
+            plant: req.data.plant,
+            material: req.data.material,
+            quantity: req.data.quantity,
+            unit: req.data.unit,
+            strgloc_frm: req.data.strgloc_frm,
+            strgloc_to: req.data.strgloc_to,
+            recip: req.data.recip,
+            unl_pnt: req.data.unl_pnt,
+            prodord: req.data.prodord,
+            linkage: req.data.linkage,
+            costcntr: req.data.costcntr,
+            glacc: req.data.glacc,
+            text: req.data.text,
+            basedt: req.data.basedt,
+            linkage: req.data.plant,
+            glacc: req.data.glacc,
+            text: req.data.text,
+            basedt: req.data.basedt,
+            linkage: req.data.plant
+        }
+        var response = await soapi.tx(req).post("/ZCDSEHPPB0097", payload);
+        return response;
     });
     that.on('READ', 'I_PlantStdVH', async req => {
         const bupa = await cds.connect.to('ZSRVBHPP0019');
@@ -29,5 +60,5 @@ var registerZAPIBPS0007Handler = function (that, cds) {
         return bupa.run(req.query);
     })
 }
- 
+
 module.exports = registerZAPIBPS0007Handler;
